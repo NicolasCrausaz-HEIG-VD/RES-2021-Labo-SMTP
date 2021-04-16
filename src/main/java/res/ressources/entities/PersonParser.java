f896f150fc28524ac271f8b07984b0f485a23fe9
Nicolas Crausaz <n.crausaz99@gmail.com>
2021-04-16 18:12:27 +0200

    PersonParser, need to finish preparePrank

---
+++ src/main/java/res/ressources/entities/PersonParser.java
@@ -0,0 +1,33 @@
+package res.ressources.entities;
+
+import res.ressources.config.ConfigManager;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.LinkedList;
+
+public class PersonParser
+{
+    public static LinkedList<Person> getPersons()
+    {
+        LinkedList<Person> personsList = new LinkedList<>();
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(ConfigManager.getInstance().getPranksFile())))
+        {
+            String line = reader.readLine();
+
+            while (line != null)
+            {
+                personsList.add(new Person(line));
+                line = reader.readLine();
+            }
+
+        } catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+
+        return personsList;
+    }
+}
--- src/main/java/res/ressources/pranks/PrankGenerator.java
+++ src/main/java/res/ressources/pranks/PrankGenerator.java
@@ -1,8 +1,11 @@
 package res.ressources.pranks;
 
+import com.sun.tools.classfile.RuntimeTypeAnnotations_attribute;
+import res.ressources.config.ConfigManager;
 import res.ressources.entities.Group;
 import res.ressources.entities.Mail;
 import res.ressources.entities.Person;
+import res.ressources.entities.PersonParser;
 
 import java.util.LinkedList;
 import java.util.Random;
@@ -32,11 +35,35 @@ public class PrankGenerator
 
     public LinkedList<Mail> preparePranksMails()
     {
+        LinkedList<Person> persons = PersonParser.getPersons();
         LinkedList<Mail> mails = new LinkedList<>();
+        final int NB_PEOPLE_IN_GROUP = persons.size() / ConfigManager.getInstance().getNumberOfGroups();
+
+        if (NB_PEOPLE_IN_GROUP < 2)
+        {
+            throw new RuntimeException("Not enough emails to create " + ConfigManager.getInstance().getNumberOfGroups() + "groups of at least 2 people.");
+        }
+
+        // Faire des ranges
+        //LinkedList<Group> mails = new LinkedList<>();
+
+        // TODO: Create groups of number people, add them to list
+/*
+        for (int i = 0; i < persons.size(); i++)
+        {
+            if (i % NB_PEOPLE_IN_GROUP == 0)
+            {
+
+            }
+
+        }
+*/
+
         for (Prank p : pranks)
         {
             // Group gr = new Group(); // TODO: Create a mail parser
             // mails.add(new Mail(hiddenSender, new Group(new Person[]{new Person("test")}), p.getSubject(), p.getBody()));
         }
+        return null;
     }
 }
